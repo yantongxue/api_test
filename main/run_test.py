1315db70379aeb6015bb131b6b6b5a16df5279e9
yanlizhi <yanlizhi>
2019-06-20 10:07:10 +0800

    修改run.py

--- main/run_test.py
+++ main/run_test.py
@@ -1,14 +1,18 @@
-#coding=utf-8
+import sys
+import os
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+
 from data.get_data import Get_data
 from base.demo import RunMain
 from util.commen import CommonUtil
-
-from data.dependdent_data import Deppenddent_data
 from util.send_mail import SendMail
 from redis import *
 import json
 from jsonpath_rw import jsonpath,parse
-from util.operation_json import operationJson
+
+
 
 class Run_Test():
     def __init__(self):
